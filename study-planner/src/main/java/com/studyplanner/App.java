05025b50e91b7dd3e88e8024710b66ba75a5327c
Lintondsouza <ldf028@@uregina.ca>
2025-07-21 19:16:20 -0600

    y

---
+++ study-planner/src/main/java/com/studyplanner/App.java
@@ -0,0 +1,8 @@
+package com.studyplanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Study Planner!");
+        // Your logic here
+    }
+}
---
+++ study-planner/src/test/java/com/studyplanner/AppTest.java
@@ -0,0 +1,11 @@
+package com.studyplanner;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testMain() {
+        assertTrue(true); // Dummy test
+    }
+}
