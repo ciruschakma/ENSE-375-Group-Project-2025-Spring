b8d7e6e3576d5bbc332899b3cb8527e04b14a434
Maheen <mso783@uregina.ca>
2025-07-26 03:48:43 -0600

    Implement loading tasks from DB to pass test (TDD step 2)

--- study-planner/src/main/java/com/studyplanner/TaskDb.java
+++ study-planner/src/main/java/com/studyplanner/TaskDb.java
@@ -1,6 +1,9 @@
 package com.studyplanner;
 
 import com.mongodb.client.*;
+ import java.util.*;
+ import java.time.LocalDate;
+
 import org.bson.Document;
 
 public class TaskDB {
@@ -13,5 +16,23 @@ public class TaskDB {
                     .append("completed", t.isCompleted());
             collection.insertOne(doc);
         }
+    } 
+public static List<Task> loadAllTasks() {
+    List<Task> tasks = new ArrayList<>();
+    try (MongoClient client = MongoClients.create("mongodb://localhost:27017")) {
+        MongoDatabase db = client.getDatabase("StudyPlanner");
+        MongoCollection<Document> collection = db.getCollection("tasks");
+        for (Document doc : collection.find()) {
+            String title = doc.getString("title");
+            String dateStr = doc.getString("date");
+            boolean completed = doc.getBoolean("completed", false);
+            Task t = new Task(title, LocalDate.parse(dateStr));
+            t.setCompleted(completed);
+            tasks.add(t);
+        }
     }
+    return tasks;
+}
+
+    
 }
