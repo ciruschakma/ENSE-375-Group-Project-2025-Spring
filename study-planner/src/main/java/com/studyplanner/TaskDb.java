bf1c9f7aadba753c7156ac653147e5b754fcdc67
Maheen <mso783@uregina.ca>
2025-07-27 14:21:27 -0600

    Add target/ to .gitignore and remove build artifacts from git

--- study-planner/src/main/java/com/studyplanner/TaskDb.java
+++ study-planner/src/main/java/com/studyplanner/TaskDb.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TaskDb {
+public class TaskDB {
     private static final String DB_URL = "jdbc:sqlite:tasks.db";
 
     static {
